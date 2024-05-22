c8b50911ad327c7719f9127731af99dfe204d52f
cisco <cisco@UPOAULA10306>
2024-05-22 11:43:33 -0500

    Agregar archivos de proyecto.

---
+++ ApiBurgerDanielaMora/Data/ApiBurgerDanielaMoraContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiBurgerDanielaMora.Models;
+
+namespace ApiBurgerDanielaMora.Data
+{
+    public class ApiBurgerDanielaMoraContext : DbContext
+    {
+        public ApiBurgerDanielaMoraContext (DbContextOptions<ApiBurgerDanielaMoraContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiBurgerDanielaMora.Models.Burger> Burger { get; set; } = default!;
+    }
+}
---
+++ ApiBurgerDanielaMora/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ApiBurgerDanielaMora.Data;
+using ApiBurgerDanielaMora.Controllers;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApiBurgerDanielaMoraContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApiBurgerDanielaMoraContext") ?? throw new InvalidOperationException("Connection string 'ApiBurgerDanielaMoraContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapBurgerEndpoints();
+
+app.Run();
